74e7f37f9768b2b2700db655a1985c73a40a6e2e
RAJAT4SONI <107169186+RAJAT4SONI@users.noreply.github.com>
2022-06-09 12:02:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.header('🍌🍓Build your own fruit smoothie🥝🍇🥤🍹')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('fruit')
 
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
